f5e681e3153e51fe078aa6b9339b74f930054277
Ben H <ben@gxm.com>
2018-06-04 12:54:35 -0500

    6th Commit

--- js/main.js
+++ js/main.js
@@ -61,6 +61,19 @@
             }
         });
 
+        $('#slider4').owlCarousel({
+            loop: false,
+            margin: 10,
+            dots: false,
+            nav: true,
+            navText: ['<i class=\'fa fa-angle-left\'></i>', '<i class=\'fa fa-angle-right\'></i>'],
+            responsive: {
+                0: {
+                    items: 1
+                }
+            }
+        });
+
     };
 
 
