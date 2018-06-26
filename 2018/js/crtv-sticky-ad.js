c2e504cb2021ea79efc4a3a14dbcff219de29cf7
Ben H <ben@gxm.com>
2018-06-25 20:25:32 -0500

    2018 Election Commit 1

---
+++ 2018/js/crtv-sticky-ad.js
@@ -0,0 +1,33 @@
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function () { stick(); };
+
+setTimeout(function () { popup(); }, 1000);
+
+// Get the header
+var header = document.getElementById('sticky-ad');
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stick() {
+    if (window.pageYOffset >= sticky) {
+        header.classList.add('sticky');
+        $('#sticky-ad').show();
+        $('.gxm-cr-sharebtns').show();
+        $('.ad-popup').hide();
+    } else {
+        header.classList.remove('sticky');
+        $('#sticky-ad').hide();
+        $('.gxm-cr-sharebtns').hide();
+    }
+}
+
+function popup() {
+    $('#ad-modal').modal();
+}
+
+window.onload = function () {
+    $('#sticky-ad').hide();
+    $('.gxm-cr-sharebtns').hide();
+};
